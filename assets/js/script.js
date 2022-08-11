b41024971660c02e8d1fd365b83abdaccf3ea52a
Shawn Canavan <105885313+shawnbandy@users.noreply.github.com>
2022-08-11 19:50:11 -0400

    updated to main branch

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+/*
+*Psuedocode 
+
+TODO: create event listener for the category button pressed 
+    *Populate the main article and sub-articles with the news category type
+TODO: Populate/Fetch the side bar with the top stories API as cards, limit with 3
+    *Add refresh button to the side bar
+TODO: Po
+
+
+
+
+
+*/
